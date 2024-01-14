de258fa758ccfcba257f8b47d490291e1c3594f0
RayPlante <raymond.plante@nist.gov>
2024-01-14 09:49:32 -0500

    auth.module: allow choosing Mock service based on ang env

--- libs/oarng/src/lib/auth/auth.module.ts
+++ libs/oarng/src/lib/auth/auth.module.ts
@@ -3,6 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { AuthenticationService, OARAuthenticationService } from './auth.service';
 import { ConfigModule } from '../config/config.module';
+import { Credentials } from './auth';
+
+export function createAuthenticationService(httpcli: HttpClient, cfgSvc: ConfigurationService,
+                                            creds: Credentials = null)
+{
+    if (creds == null)
+        return new OARAuthenticationService(httpcli, cfgSvc);
+    return new MockAuthenticationService(creds)
+}
 
 /**
  * a module providing components used to build a wizard interface.
@@ -19,6 +28,22 @@ import { ConfigModule } from '../config/config.module';
     exports: [
     ]
 })
-export class AuthModule { }
+export class AuthModule {
+    /**
+     * provide a singleton AuthenticationService that can accept static mock credentials
+     */
+    public static withCredentials(Credentials creds = null) {
+        return {
+            ngModule: AuthModule,
+            imports: [
+                CommonModule, ConfigModule
+            ],
+            providers: [
+                { provide: AuthenticationService, useFactory: createAuthenticationService,
+                  deps: [ HttpClient, ConfigurationService, creds ] },
+            ]
+        };
+    }
+}
 
-export { AuthenticationService, OARAuthenticationService }
+export { AuthenticationService, OARAuthenticationService, Credentials }
